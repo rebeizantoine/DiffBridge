12e94affb77afafc5728926b73435303acb100a9
ElieGre <118279594+ElieGre@users.noreply.github.com>
2024-05-16 15:23:42 +0100

    first commit

---
+++ src/Components/Header.jsx
@@ -0,0 +1,79 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../Styles/header.css';
+import { Helmet } from 'react-helmet';
+import BrushLogo from '../Images/HighRez@3x.jpg'
+
+const Header = () => {
+  return (
+    <div>
+      <div className="header-box">
+        <Helmet>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="true"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Rubik:wght@300;400;700&display=swap"
+            rel="stylesheet"
+          />
+        </Helmet>
+        <div className="header-all">
+          <div className="header-title">
+            <img
+            src={BrushLogo}
+              className="steel-city"
+              alt=""
+            />
+          </div>
+          <div className='header-center'>
+          <div className="header-1">
+            <nav>
+              <ul className="header-ul">
+                <li>
+                  <a href="/">Home</a>
+                </li>
+                <li>
+                  <a href="">Artists</a>
+                </li>
+                <li>
+                  <a
+                    className="weird-blue"
+                    href="/#services"
+                  >
+                    Exhibitions
+                  </a>
+                </li>
+                <li>
+             
+     <a
+                    className="weird-blue"
+                    href="/#projects"
+                  >
+                    About Us
+                  </a>
+                </li>
+                <li>
+                  <a href="/contact">Contact</a>
+                </li>
+              </ul>
+            </nav>
+          </div>
+          </div>
+          <div className="header-2">
+            <div className="image-circle">
+              <img
+                className="phone-image"
+                alt=""
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
