7e754164ef26f5b2827f07401487f27c2d09a053
rebeizantoine <rebeizantoine@gmail.com>
2024-05-18 19:48:58 +0300

    added artists route

--- src/App.js
+++ src/App.js
@@ -3,18 +3,40 @@ import Header from "./Components/Header";
 import Hero from "./Components/Hero";
 import Section1 from "./Components/Section1";
 import Section2 from "./Components/Section2";
+import Artists from "./Components/Artists";
+import New from "./Components/New";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <div>
-        <Header />
-        <Hero />
-        <Section1 />
-        <Section2 />
-        <title>Bridging Brushes</title>
-      </div>
+      <Router>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <Header />
+                <Hero />
+                <Section1 />
+                <Section2 />
+                <New />
+                <title>Bridging Brushes</title>
+              </>
+            }
+          />
+          <Route
+            path="/artists"
+            element={
+              <>
+                <Header />
+                <Artists />
+              </>
+            }
+          />
+        </Routes>
+      </Router>
+      <div></div>
     </div>
   );
 }
