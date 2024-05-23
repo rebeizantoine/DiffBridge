bfb0f1faf9e651f25e66e5ec01da68dfb2eebfab
rebeizantoine <rebeizantoine@gmail.com>
2024-05-24 01:14:48 +0300

    123

--- src/Components/RealFooter.jsx
+++ src/Components/RealFooter.jsx
@@ -1,12 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import "../Styles/realfooter.css";
 import verticalwhite from "../Images/vertical-lign-white.png";
 import facebook from "../Images/facebook.png";
 import pinterest from "../Images/pinterest.png";
 import instagram from "../Images/instagram.png";
 import youtube from "../Images/youtube.png";
+import facebookGif from "../Images/icons8-facebook.gif"; // replace with the actual path
+import pinterestGif from "../Images/icons8-pinterest.gif"; // replace with the actual path
+import instagramGif from "../Images/icons8-instagram.gif"; // replace with the actual path
+import youtubeGif from "../Images/icons8-youtube.gif"; // replace with the actual path
 
 const RealFooter = () => {
+  const [hoveredEmoji, setHoveredEmoji] = useState({
+    facebook: false,
+    pinterest: false,
+    instagram: false,
+    youtube: false,
+  });
+
+  const handleMouseEnter = (emoji) => {
+    setHoveredEmoji((prevState) => ({
+      ...prevState,
+      [emoji]: true,
+    }));
+  };
+
+  const handleMouseLeave = (emoji) => {
+    setHoveredEmoji((prevState) => ({
+      ...prevState,
+      [emoji]: false,
+    }));
+  };
   return (
     <div className="all-footer">
       <div className="footer1">
@@ -36,28 +60,52 @@ const RealFooter = () => {
         <div className="emojis">
           <nav>
             <li>
-              <div className="white-emoji">
-                {" "}
-                <img src={facebook} alt="" />
-              </div>{" "}
+              <div
+                className="white-emoji"
+                onMouseEnter={() => handleMouseEnter("facebook")}
+                onMouseLeave={() => handleMouseLeave("facebook")}
+                style={{
+                  backgroundImage: `url(${
+                    hoveredEmoji.facebook ? facebookGif : facebook
+                  })`,
+                }}
+              ></div>
             </li>
             <li>
-              <div className="white-emoji">
-                {" "}
-                <img src={pinterest} alt="" />
-              </div>
+              <div
+                className="white-emoji"
+                onMouseEnter={() => handleMouseEnter("pinterest")}
+                onMouseLeave={() => handleMouseLeave("pinterest")}
+                style={{
+                  backgroundImage: `url(${
+                    hoveredEmoji.pinterest ? pinterestGif : pinterest
+                  })`,
+                }}
+              ></div>
             </li>
             <li>
-              <div className="white-emoji">
-                {" "}
-                <img src={instagram} alt="" />
-              </div>
+              <div
+                className="white-emoji"
+                onMouseEnter={() => handleMouseEnter("instagram")}
+                onMouseLeave={() => handleMouseLeave("instagram")}
+                style={{
+                  backgroundImage: `url(${
+                    hoveredEmoji.instagram ? instagramGif : instagram
+                  })`,
+                }}
+              ></div>
             </li>
             <li>
-              <div className="white-emoji">
-                {" "}
-                <img src={youtube} alt="" />
-              </div>{" "}
+              <div
+                className="white-emoji"
+                onMouseEnter={() => handleMouseEnter("youtube")}
+                onMouseLeave={() => handleMouseLeave("youtube")}
+                style={{
+                  backgroundImage: `url(${
+                    hoveredEmoji.youtube ? youtubeGif : youtube
+                  })`,
+                }}
+              ></div>
             </li>
           </nav>
         </div>
@@ -66,9 +114,11 @@ const RealFooter = () => {
             Stay up-to-date on Bridging Brushes artists, exhibitions,and events.
           </p>
         </div>
-        <div className="subscribe-footer">
-          <button>SUBSCRIBE</button>
-        </div>
+        {/* <div className="subscribe-footer"> */}
+        <button className="button">
+          <span className="span123">SUBSCRIBE</span>
+        </button>
+        {/* </div> */}
       </div>
     </div>
   );
